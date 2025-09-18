7ca5b6aa9148493c145ae4c9271fab3a55be093c
BP <valkarian@valkarian.com>
2025-09-18 10:42:58 +0200

    Initial commit

---
+++ src/main/java/com/example/cadettsplitterssushi/CadettsplitterssushiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadettsplitterssushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadettsplitterssushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadettsplitterssushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cadettsplitterssushi/CadettsplitterssushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadettsplitterssushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadettsplitterssushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
